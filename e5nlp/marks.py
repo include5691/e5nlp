e61524721e524366c35109aac0cc33f0b9d22947
Andrey Pshenitsyn <include5691@gmail.com>
2025-01-16 17:16:03 +0500

    chore[marks]: chane args typing

--- e5nlp/marks.py
+++ e5nlp/marks.py
@@ -2,7 +2,7 @@ import random
 
 WELCOME_LIST = ["Здравствуйте", "Приветствую", "Доброго дня"]
 
-def inject_marks(text: str, customer_name: str = "", car_name: str = "", user_name: str = "") -> str:
+def inject_marks(text: str, customer_name: str | None = None, car_name: str | None = None, user_name: str | None = None) -> str:
     """
     - {w} : welcome item
     - {n} : customer name
@@ -13,11 +13,17 @@ def inject_marks(text: str, customer_name: str = "", car_name: str = "", user_na
     if customer_name:
         if text[:3] != "{n}":
             customer_name = f", {customer_name}"
+    else:
+        customer_name = ""
     if car_name:
         car_name = f", {car_name}"
+    else:
+        car_name = ""
     if user_name:
         parts = user_name.split(" ")
         if len(parts) > 1:
             user_name = parts[1]
+    else:
+        user_name = ""
     welcome_item = random.choice(WELCOME_LIST)
     return text.format(w=welcome_item, n=customer_name, c=car_name, u=user_name).replace("  ", " ").replace(" ,", ",").replace(",,", ",").replace(", , ", ", ")
